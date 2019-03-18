5c838f4172d19765a4315884df065a65af2bd47b
Kalisti <jann@mistrust.net>
2019-03-18 10:05:12 +0100

    New utility function for private key creation

--- lib/acme/distributed/util.rb
+++ lib/acme/distributed/util.rb
@@ -1,3 +1,6 @@
+require 'openssl'
+require 'acme/distributed/errors'
+
 # Utility functions for Acme::Distributed
 #
 module Acme::Distributed::Util
@@ -14,6 +17,11 @@ module Acme::Distributed::Util
   # If given, the options hash further defines the behaviour of the replacement
   # actions.
   #
+  # @param string [String] The original string
+  # @param varset [Hash] Hash containing variable-value mappings for variables to be interpolated
+  # @param options [Hash] Hash containing replacement options
+  # @return [String] A copy of string with all variables being replaced
+  #
   def self.expand_variables(string, varset, options = {})
     raise TypeError, "varset must be Hash" if not varset.is_a?(Hash)
     raise TypeError, "options must be Hash" if not options.is_a?(Hash)
@@ -28,5 +36,22 @@ module Acme::Distributed::Util
       end
     end
     result
+  end
+
+  # Generate a private RSA key and stores it at path specified. The path where
+  # the key should be stored must exist and be writable for the current user.
+  # 
+  # @throws [Acme::Distributed::ConfigurationError] Path does not exist or is not writable
+  # @return nothing
+  #
+  def self.generate_private_key!(path, umask = 0077)
+    basepath = File.dirname(path)
+    if not File.writable?(basepath)
+      raise Acme::Distributed::ConfigurationError, "Path #{basepath} does either not exist or is not writeable."
+    end
+    key = OpenSSL::PKey::RSA.new(4096)
+    orig_umask = File.umask(umask)
+    File.write(path, key, 0, mode: "w")
+    File.umask(orig_umask)
   end
 end
